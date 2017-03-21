6b5a6ee7c35213c00ce7d1c197723f2d9c3aec8f
SEAGATE\658284 <benjamin.wiriyapong@seagate.com>
2017-03-21 17:02:27 +0700

    updated eq_wdc.js

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -1,3 +1,18 @@
+//basic function 
+(function () {
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) {
+
+    };
+
+    myConnector.getData = function (table, doneCallback) {
+
+    };
+
+    tableau.registerConnector(myConnector);
+})();
+
 $(document).ready(function () {
     $("#submitButton").click(function () {
         tableau.connectionName = "USGS Earthquake Feed";
